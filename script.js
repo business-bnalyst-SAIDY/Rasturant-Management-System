230d9671c1687d812841d37de2610bc396c1655b
business-bnalyst-SAIDY <saidy15-4501@diu.edu.bd>
2025-03-21 00:08:04 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('exploreButton').addEventListener('click', function() {
+    alert('You clicked the explore button!');
+    // You can add more JavaScript functionality here
+});
